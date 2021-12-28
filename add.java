5591b3bf623ec3fde2798ab3844c9a2d6b39c226
Omen <omcarsawant@gmail.com>
2021-12-28 11:47:36 +0530

    this is new repo files

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add{
+
+         public static void main(String[] args){
+
+              int x=10, y=5,s=0;
+
+              s=x+y;
+
+              System.out.println("addition is :"+s);
+         }
+}
---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello{
+
+
+
+       public static void main(String[] args){
+           
+            for(int i=1;i<=15;i++){
+                  System.out.println("hello there.."+i);
+
+                   }
+        }
+}
